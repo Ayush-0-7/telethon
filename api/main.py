52627574b5b21d8ff293b0391e4f182d8f1c0313
Ayush-0-7 <22052720@kiit.ac.in>
2024-08-19 00:57:30 +0530

    11th commit

--- api/main.py
+++ api/main.py
@@ -28,19 +28,7 @@ def main():
 client = TelegramClient('session_name', API_ID, API_HASH)
 
 # Initialize Firebase
-cred = credentials.Certificate({
-  "type": "service_account",
-  "project_id": "lootcase-5f198",
-  "private_key_id": PVT_ID,
-  "private_key": PVT_KEY.replace('\\n', '\n'),
-  "client_email": "firebase-adminsdk-uumst@lootcase-5f198.iam.gserviceaccount.com",
-  "client_id": "102461053564389743283",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-uumst%40lootcase-5f198.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-})
+cred = credentials.Certificate("credentials.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 doc_ref = db.collection('lootndeals')
